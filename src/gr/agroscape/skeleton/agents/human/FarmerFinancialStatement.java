6b59b426bd40e6995d325719d543d4bbcfd9b6db
dkremmydas <kremmydas@aua.gr>
2015-11-05 14:23:23 +0200

    untracked many not necessary files

---
+++ src/gr/agroscape/skeleton/agents/human/FarmerFinancialStatement.java
@@ -0,0 +1,14 @@
+package gr.agroscape.skeleton.agents.human;
+
+/**
+ * A base class for representing the financial status of a Farmer
+ *  
+ * @author Dimitris Kremmydas
+ * @version $Revision$
+ * @since 2.0
+ */
+public class FarmerFinancialStatement {
+
+	private Long liquidity;
+	
+}
