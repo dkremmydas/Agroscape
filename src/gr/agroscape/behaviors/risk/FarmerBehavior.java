3e4006099fa7badf3218d3913bc5eb5e3059e164
Dimitris Kremmydas <jkr@agribusinessLAP>
2016-03-16 14:13:54 +0200

    writing utility and prospect

--- src/gr/agroscape/behaviors/risk/FarmerBehavior.java
+++ src/gr/agroscape/behaviors/risk/FarmerBehavior.java
@@ -4,6 +4,9 @@ import gr.agroscape.behaviors.Behavior;
 import gr.agroscape.behaviors.BehaviorFactory;
 import gr.agroscape.skeleton.agents.AgroscapeAgent;
 import gr.agroscape.skeleton.agents.human.Farmer;
+
+import java.util.ArrayList;
+
 import repast.simphony.context.Context;
 
 public class FarmerBehavior extends Behavior{
@@ -29,6 +32,8 @@ public class FarmerBehavior extends Behavior{
 	public void budgetConstraintCalculation() {
 		this.budgetConstraint = f.getAccount().getCash() / 2;
 	}
+	
+	
 
 	/**
 	 * Getter
@@ -36,6 +41,47 @@ public class FarmerBehavior extends Behavior{
 	 */
 	public long getBudgetConstraint() {
 		return budgetConstraint;
+	}
+	
+	
+	
+	class Prospect {
+		// 0: -80%, 1: -50%, 2: -25%, 3: -1-%
+		// 4: 0, 5: +10%, 6: +20%, 7: +50% 8: +60%, 9: +80%
+		private double probs[] = new double[9];
+		private double incomeDiffs[] = {-0.8,-0.5,-0.25,-0.1,0.0,0.1,0.25,0.35,0.5,0.8};
+
+		public Prospect() {
+			//initialize all with equal probability
+			for(int i=0;i<9;i++) this.probs[i] = .1;
+		}
+		
+		public void update(ArrayList<Long> history) {
+			
+		}
+		
+		public double getExpectedIncomeDiff() {
+			double est = 0 ;
+			for(int i=0;i<9;i++) {
+				est =+ (probs[i]*incomeDiffs[i]);
+			}		
+			return est;
+		}
+		
+	}
+	
+	/**
+	 * From 0 - 100
+	 * 
+	 * @author Dimitris Kremmydas
+	 *
+	 */
+	class Utility {
+		
+		
+		public int getUtility(Prospect p) {
+			return 100;
+		}
 	}
 	
 	
