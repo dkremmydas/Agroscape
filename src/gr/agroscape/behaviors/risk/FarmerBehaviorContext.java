6a286439673378ba19b473aaec3c7153c03b0eea
Dimitris Kremmydas <jkr@agribusinessLAP>
2016-03-16 13:47:50 +0200

    finished farmerscontext

--- src/gr/agroscape/behaviors/risk/FarmerBehaviorContext.java
+++ src/gr/agroscape/behaviors/risk/FarmerBehaviorContext.java
@@ -4,7 +4,7 @@ import gr.agroscape.behaviors.BehaviorContext;
 
 public class FarmerBehaviorContext extends BehaviorContext {
 	
-	private OutpurPriceGenerator pg;
+	private OutpurPriceGenerator pg = new OutpurPriceGenerator();
 	private long currentPrice;
 	private int period = 0;
 	
@@ -16,12 +16,20 @@ public class FarmerBehaviorContext extends BehaviorContext {
 		return currentPrice;
 	}
 
+	public double drawCurrentPrice() {
+		return (double)currentPrice;
+	}
 	
+	public int getPeriod() {
+		return period;
+	}
 	
 	//scheduled to run every 10
 	public void calculateNextPrice() {
 		this.currentPrice = pg.getPrice(period);
 		this.period++;
 	}
+
+
 
 }
